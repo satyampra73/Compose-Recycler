df851a30a9b6a155895162c801fd7bea5ccdffc7
Satyam Prajapati <satyam7355pra@gmail.com>
2025-02-15 17:56:39 +0530

    clickable simple recycler view created

--- app/src/main/java/com/satyam/composerecycler/MainActivity.kt
+++ app/src/main/java/com/satyam/composerecycler/MainActivity.kt
@@ -1,47 +1,87 @@
 package com.satyam.composerecycler
 
+import android.R.attr.horizontalDivider
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.satyam.composerecycler.ui.theme.ComposeRecyclerTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            ComposeRecyclerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+//            ScrollableColumnDemo()
+//            LazyColumnDemo()
+            LazyColumnDemoClickable {
+                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
             }
+
+        }
+    }
+
+}
+@Composable
+fun ScrollableColumnDemo() {
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier.verticalScroll(scrollState)
+    ) {
+        for (i in 1..100){
+            Text(
+                "User Name $i",
+                style = MaterialTheme.typography.headlineLarge,
+                modifier = Modifier.padding(10.dp)
+            )
+            HorizontalDivider(color = Color.Black, thickness = 5.dp)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeRecyclerTheme {
-        Greeting("Android")
+fun LazyColumnDemo() {
+    LazyColumn {
+        items(100) {
+            Text(
+                "User Name $it",
+                style = MaterialTheme.typography.headlineLarge,
+                modifier = Modifier.padding(10.dp)
+            )
+            HorizontalDivider(color = Color.Black, thickness = 5.dp)
+        }
     }
 }
+
+@Composable
+fun LazyColumnDemoClickable(selectedItem: (String) -> (Unit)) {
+    LazyColumn {
+        items(100) {
+            Text(
+                "User Name $it",
+                style = MaterialTheme.typography.headlineLarge,
+                modifier = Modifier.padding(10.dp)
+                    .fillMaxWidth()
+                    .clickable{ selectedItem("$it selecetd")}
+            )
+            HorizontalDivider(color = Color.Black, thickness = 5.dp)
+        }
+    }
+}
+
