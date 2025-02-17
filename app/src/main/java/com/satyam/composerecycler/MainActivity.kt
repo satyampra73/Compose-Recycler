4c8dd15bea17801141e79ab612de0deebcd57b33
Satyam Prajapati <satyam7355pra@gmail.com>
2025-02-17 17:58:23 +0530

    Recycler View Created with static image and content

--- app/src/main/java/com/satyam/composerecycler/MainActivity.kt
+++ app/src/main/java/com/satyam/composerecycler/MainActivity.kt
@@ -2,26 +2,47 @@ package com.satyam.composerecycler
 
 import android.R.attr.horizontalDivider
 import android.os.Bundle
+import android.view.RoundedCorner
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-
+import com.satyam.composerecycler.data.TvShowList
+import com.satyam.composerecycler.data.TvShowList.tvShows
+import com.satyam.composerecycler.model.TvShow
 
 
 class MainActivity : ComponentActivity() {
@@ -30,21 +51,26 @@ class MainActivity : ComponentActivity() {
         setContent {
 //            ScrollableColumnDemo()
 //            LazyColumnDemo()
-            LazyColumnDemoClickable {
-                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+//            LazyColumnDemoClickable {
+//                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+//            }
+
+            DisplayTvShows {
+                Toast.makeText(this, it.name, Toast.LENGTH_SHORT).show()
             }
 
         }
     }
 
 }
+
 @Composable
 fun ScrollableColumnDemo() {
     val scrollState = rememberScrollState()
     Column(
         modifier = Modifier.verticalScroll(scrollState)
     ) {
-        for (i in 1..100){
+        for (i in 1..100) {
             Text(
                 "User Name $i",
                 style = MaterialTheme.typography.headlineLarge,
@@ -76,12 +102,91 @@ fun LazyColumnDemoClickable(selectedItem: (String) -> (Unit)) {
             Text(
                 "User Name $it",
                 style = MaterialTheme.typography.headlineLarge,
-                modifier = Modifier.padding(10.dp)
+                modifier = Modifier
+                    .padding(10.dp)
                     .fillMaxWidth()
-                    .clickable{ selectedItem("$it selecetd")}
+                    .clickable { selectedItem("$it selecetd") }
             )
             HorizontalDivider(color = Color.Black, thickness = 5.dp)
         }
     }
+}
+
+//Displaying TvShow list
+
+@Composable
+fun DisplayTvShows(selectedItem: (TvShow) -> Unit) {
+    val tvShowsList = TvShowList.tvShows
+    LazyColumn(
+        contentPadding = PaddingValues(horizontal = 8.dp, vertical = 8.dp)
+    ) {
+        items(tvShowsList) {
+            it->
+            TvShowListItem(tvShow = it, selecteditem = selectedItem)
+        }
+    }
+}
+
+
+
+@Composable
+fun TvShowListItem(tvShow: TvShow, selecteditem: (TvShow) -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(5.dp),
+        shape = RoundedCornerShape(corner = CornerSize(10.dp))
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(5.dp)
+                .fillMaxWidth()
+                .clickable {
+                    selecteditem(tvShow)
+                },
+            verticalAlignment = Alignment.CenterVertically
+        )
+        {
+            TvShowImage(tvShow = tvShow)
+            Column {
+                Text(text = tvShow.name, style = MaterialTheme.typography.headlineMedium)
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = tvShow.overview, style = MaterialTheme.typography.bodySmall,
+                    maxLines = 3, overflow = TextOverflow.Ellipsis)
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Row (
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+
+                    Text(text = tvShow.year.toString(), style = MaterialTheme.typography.bodySmall)
+                    Text(text = tvShow.rating.toString(), style = MaterialTheme.typography.bodySmall)
+
+                }
+
+            }
+
+        }
+
+
+    }
+}
+
+
+@Composable
+private fun TvShowImage(tvShow: TvShow) {
+    Image(
+        painter = painterResource(id = tvShow.imageId),
+        contentDescription = null,
+        modifier = Modifier
+            .padding(4.dp)
+            .height(140.dp)
+            .width(100.dp)
+            .clip(RoundedCornerShape(corner = CornerSize(10.dp))),
+        contentScale = ContentScale.Crop,
+
+        )
 }
 
